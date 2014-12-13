53d2580ffee7c096eb576909195fb9d97f837c5c
paulzfm <zhufengminpaul@163.com>
2014-12-13 20:39:34 +0800

    only time cost right now

--- autograder.py
+++ autograder.py
@@ -4,52 +4,79 @@ import filecmp
 import datetime
 
 class Grader:
-	def run(self, sample):
-		stdoutput = os.path.join(self.outputFolder, '%s_std.out' % (os.path.basename(sample)))
-		ret1 = os.system('%s < %s > %s' % (self.std, sample, stdoutput))
-		ret1 /= 256
-		if ret1 != 0:
-			print '*** Error: Stardard program exit with code %i' % ret1
-			sys.exit(1)
-		
-		myoutput = os.path.join(self.outputFolder, '%s_my.out' % (os.path.basename(sample)))
-		start = datetime.datetime.now()
-		ret2 = os.system('%s < %s > %s' % (self.my, sample, myoutput))
-		end = datetime.datetime.now()
-		t = (end - start).microseconds
-		ret2 /= 256
-		if ret2 != 0:
-			return ('Runtime Error: %i' % ret2, time)
+	def runStdProgram(self):
+		count = 0
+		for sample in self.samples:
+			count += 1
+			print 'Running standard program (%i/%i) ...' % (count, len(self.samples))
+			stdoutput = os.path.join(self.outputFolder, '%s.stdout' % (os.path.basename(sample)))
+			ret = os.system('%s < %s > %s' % (self.std, sample, stdoutput))
+			ret /= 256
+			if ret != 0:
+				print '*** Error: Standard program exit with code %i' % ret
+				sys.exit(1)
 
-		if filecmp.cmp(stdoutput, myoutput):
-			return ('Correct', t)
-		else:
-			return ('Wrong', t)
+	def test(self):
+		print '\nBegin test...\n'
+		count = 0
+		for sample in self.samples:
+			count += 1
+			myoutput = os.path.join(self.outputFolder, '%s.out' % (os.path.basename(sample)))
+			start = datetime.datetime.now()
+			ret = os.system('%s < %s > %s' % (self.my, sample, myoutput))
+			end = datetime.datetime.now()
+			tcost = (end - start).microseconds
+			tcost /= 1000.0
+			result = ''
+			ret /= 256
+			if ret != 0:
+				result = 'Runtime Error: %i' % ret
+
+			stdoutput = os.path.join(self.outputFolder, '%s.stdout' % (os.path.basename(sample)))
+			if filecmp.cmp(stdoutput, myoutput):
+				result = 'OK :)'
+			else:
+				result = 'Wrong'
+
+			print '%i\t[%s]\t%s\t%.2fms' % (count, os.path.basename(sample), result, tcost)
 
 	def ctrl(self):
 		self.samples = []
 		ignore = 0
 		for f in os.listdir(self.sampleFolder):
 			path = os.path.join(self.sampleFolder, f)
-			if os.path.isfile(path) and (not f.startswith('.')):
+			if os.path.isfile(path) and not f.startswith('.') and not f.endswith('~'):
 				self.samples.append(path)
 			else:
 				ignore += 1
 
-		print '%i samples found (%i ignored).' % (len(self.samples), ignore) 
+		print '%i samples found (%i ignored).' % (len(self.samples), ignore)
 
-		count = 0
-		for sample in self.samples:
-			count += 1
-			results = self.run(sample)
-			print '%i\t[%s]\t\t%s\t\t%.1fms' % (count, os.path.basename(sample), results[0], results[1] / 1000.0)
+		self.runStdProgram()
+		self.test()
 
 
 	def __init__(self, args):
 		self.std = args[1]
 		self.my = args[2]
 		self.sampleFolder = args[3]
-		self.outputFolder = args[4]
+		if len(args) == 5:
+			self.outputFolder = args[4]
+		else:
+			self.outputFolder = 'output/'
+
+		if not os.path.exists(self.std):
+			print '***Error: Standard program [%s] not found.' % self.std
+			sys.exit(1)
+
+		if not os.path.exists(self.my):
+			print '***Error: Your program [%s] not found.' % self.my
+			sys.exit(1)
+
+		if not os.path.exists(self.sampleFolder):
+			print '***Error: Sample folder [%s] not found.' % self.sampleFolder
+			sys.exit(1)
+
 		if not os.path.exists(self.outputFolder):
 			os.mkdir(self.outputFolder)
 
@@ -57,8 +84,8 @@ class Grader:
 
 
 if __name__ == '__main__':
-	if len(sys.argv) != 5:
-		print 'Usage: python %s stdProgram myProgram sampleFolder outputFolder' % sys.argv[0]
+	if len(sys.argv) != 4 and len(sys.argv) != 5:
+		print 'Usage: python %s stdProgram myProgram sampleFolder [outputFolder]' % sys.argv[0]
 		sys.exit(1)
 
 	grader = Grader(sys.argv)
